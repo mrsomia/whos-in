85a4593a99a17b67977f1a40900705e618854539
Sachin Somia <sachinsomia@gmail.com>
2023-04-28 19:51:24 +0100

    Feat: Creates .env validation file

---
+++ src/env.ts
@@ -0,0 +1,15 @@
+import { z } from "zod"
+
+const myEnv = z.object({
+	"DATABASE_URL": z.string(),
+	"SHADOW_DATABASE_URL": z.string(),
+})
+
+myEnv.parse(process.env)
+
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv extends z.infer<typeof myEnv> {}
+	}
+}
+
