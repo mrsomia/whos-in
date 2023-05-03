e4055ec1413f5d97e2a1f4b6f4142d5ff9de877d
Sachin Somia <sachinsomia@gmail.com>
2023-05-03 23:14:17 +0100

    Feat: Updates dashboard to read user

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { useUser } from "$/hooks/auth";
+import { useRouter } from "next/navigation";
+
 export default function Page() {
+  const router = useRouter();
+  const user = useUser();
+  if (!user) {
+    router.push("/signin");
+    throw new Error("Should not reach hear as there is no user");
+  }
+
   return (
     <main>
-      <h1>signed in as: unknown</h1>
+      <h1>signed in as: {user}</h1>
     </main>
   );
 }
