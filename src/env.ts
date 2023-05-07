2d7f687dcc99c66219ebb01fbd830166f0c81218
Sachin Somia <sachinsomia@gmail.com>
2023-05-07 22:59:27 +0100

    Chore: Updates for Discord env vars

--- src/env.ts
+++ src/env.ts
@@ -3,12 +3,14 @@ import { z } from "zod";
 const myEnv = z.object({
   DATABASE_URL: z.string(),
   SHADOW_DATABASE_URL: z.string(),
+  DISCORD_CLIENT_SECRET: z.string(),
+  DISCORD_CLIENT_ID: z.string()
 });
 
 myEnv.parse(process.env);
 
 declare global {
   namespace NodeJS {
-    interface ProcessEnv extends z.infer<typeof myEnv> {}
+    interface ProcessEnv extends z.infer<typeof myEnv> { }
   }
 }
