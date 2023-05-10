efb3ad12419a79343363b388e0ecbef34363ad9a
Sachin Somia <sachinsomia@gmail.com>
2023-05-10 22:13:18 +0100

    Chore: linting

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,14 @@
+import { db } from "$/lib/db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { db } from "$/lib/db"
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
   providers: [
     DiscordProvider({
-    clientId: process.env.DISCORD_CLIENT_ID,
-    clientSecret: process.env.DISCORD_CLIENT_SECRET,
-  })]
-}
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+  ],
+};
