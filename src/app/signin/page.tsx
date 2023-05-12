6a31819e46ad64b63b3c9301e3f63bfa8bd69a54
Sachin Somia <sachinsomia@gmail.com>
2023-05-12 22:23:27 +0100

    Feat: Adds email and magic link signin

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import { signIn } from "next-auth/react";
+import { useState } from "react";
 
 export default function SignIn() {
+  const [email, setEmail] = useState("")
   return (
     <div className="flex gap-8 h-48 flex-col items-center justify-center">
       <button onClick={() => signIn("discord")}>Sign in with Discord</button>
+      
+      <div>
+        <label htmlFor="email">email</label>
+        <input type="email" id="email" onChange={(e)=> setEmail(e.target.value)} value={email} />
+        <button onClick={() => signIn("email", {email})}>Sign in with email</button>
+      </div>
     </div>
   );
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { db } from "$/lib/db";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import EmailProvider from "next-auth/providers/email";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
@@ -9,6 +10,18 @@ export const authOptions: NextAuthOptions = {
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        }
+      },
+      from: process.env.EMAIL_FROM,
+      maxAge: 1* 60 * 60, // How long email links are valid for (1hr)
     }),
   ],
 };
