1a7377b4bea196bd4a1b60915bbda6ef14b32764
Sachin Somia <sachinsomia@gmail.com>
2023-05-16 23:40:50 +0100

    Fix: Adds session to provider

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -1,8 +1,15 @@
 "use client";
 
+import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import React from "react";
 
-export default function Provider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+export default function Provider({
+  session,
+  children,
+}: {
+  children: React.ReactNode;
+  session: Session | null | undefined;
+}) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
