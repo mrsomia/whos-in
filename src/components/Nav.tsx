3f14621206d03164de6650a8d3effa3ab68bc283
Sachin Somia <sachinsomia@gmail.com>
2023-05-22 23:40:25 +0100

    Fix(Nav): Styles and moves to radix

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,26 +1,37 @@
 "use client";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import { Home, LogOut } from "lucide-react";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Nav() {
   const { status } = useSession();
   return (
-    <nav className="w-100 m-auto my-8">
-      <ul className=" w-100 mx-8 my-12 flex list-none justify-between md:mx-auto md:max-w-2xl">
-        <li>Home</li>
+    <NavigationMenu.Root className="w-100 m-auto my-8">
+      <NavigationMenu.List className=" w-100 mx-8 my-12 flex list-none justify-between md:mx-auto md:max-w-2xl">
+        <NavigationMenu.Item>
+          <NavigationMenu.Link asChild>
+            <Link href={status === "unauthenticated" ? "/" : "/dashboard"}>
+              <Home />
+            </Link>
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
         {status === "unauthenticated" ? (
-          <li>
-            <button className="rounded-xl bg-orange-600 p-4 px-10 hover:bg-orange-700">
+          <NavigationMenu.Item className="rounded-xl bg-orange-600 p-2 px-6 hover:bg-orange-700">
+            <NavigationMenu.Link asChild>
               <Link href="/signin">Sign In</Link>
-            </button>
-          </li>
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
         ) : (
-          <li>
-            {" "}
-            <button onClick={() => signOut()}>Sign out</button>
-          </li>
+          <NavigationMenu.Item>
+            <LogOut
+              onClick={() => signOut()}
+              // className="rounded-xl border-2 border-orange-600 p-2 px-6 hover:border-orange-700"
+              color="white"
+            />
+          </NavigationMenu.Item>
         )}
-      </ul>
-    </nav>
+      </NavigationMenu.List>
+    </NavigationMenu.Root>
   );
 }
