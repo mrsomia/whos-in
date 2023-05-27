8461d8414b3874b7fe2486d3e948294f5c495d6a
Sachin Somia <sachinsomia@gmail.com>
2023-05-27 20:25:01 +0100

    Styles(sign-in): updates styling and formatting

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { signIn, useSession } from "next-auth/react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+
+import DiscordLogo from "../../../public/discord-mark-white.png";
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
@@ -12,11 +15,25 @@ export default function SignIn() {
     router.push("/dashboard");
   }
   return (
-    <div className="flex h-48 flex-col items-center justify-center gap-8">
-      <button onClick={() => signIn("discord")}>Sign in with Discord</button>
+    <div className="flex flex-col items-center justify-center gap-8">
+      <button
+        className="min-w-60 flex items-center justify-center gap-4 rounded-md bg-[#5865F2] p-4 px-8"
+        onClick={() => signIn("discord")}
+      >
+        <div className="">
+          <Image src={DiscordLogo} height={38} width={36} alt="discord logo" />
+        </div>
+        <div>
+          <span className="font-semibold">Sign in with Discord</span>
+        </div>
+      </button>
 
-      <div className="flex flex-col gap-4">
-        <label htmlFor="email">Email</label>
+      <div className="h-1 border-gray-100"></div>
+
+      <div className="flex flex-col text-center">
+        <label htmlFor="email" className="text-lg font-bold">
+          Email
+        </label>
         <input
           type="email"
           id="email"
