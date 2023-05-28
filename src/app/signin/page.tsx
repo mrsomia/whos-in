c7148b0e7772173a692c35bf0ac0f59f9f06180e
Sachin Somia <sachinsomia@gmail.com>
2023-05-28 22:10:35 +0100

    Fix(signin): Moves form to wrap around email signup

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -17,27 +17,27 @@ export default function SignIn() {
   }
   return (
     <main className="flex min-h-[65vh] items-center justify-center md:my-16 md:min-h-full">
-      <Form.Root>
-        <div className="flex flex-col items-center justify-center gap-4">
-          <button
-            className="min-w-60 flex items-center justify-center gap-4 rounded-md bg-[#5865F2] p-4 px-8"
-            onClick={() => signIn("discord")}
-          >
-            <div className="">
-              <Image
-                src={DiscordLogo}
-                height={38}
-                width={36}
-                alt="discord logo"
-              />
-            </div>
-            <div>
-              <span className="font-semibold">Sign in with Discord</span>
-            </div>
-          </button>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <button
+          className="min-w-60 flex items-center justify-center gap-4 rounded-md bg-[#5865F2] p-4 px-8"
+          onClick={() => signIn("discord")}
+        >
+          <div className="">
+            <Image
+              src={DiscordLogo}
+              height={38}
+              width={36}
+              alt="discord logo"
+            />
+          </div>
+          <div>
+            <span className="font-semibold">Sign in with Discord</span>
+          </div>
+        </button>
 
-          <hr className="w-100 m-8 h-1 border-t border-white bg-white" />
+        <hr className="w-100 m-8 h-1 border-t border-white bg-white" />
 
+        <Form.Root>
           <div className="flex flex-col">
             <Form.Field name="email" className="m-4 flex flex-col gap-4">
               <div className="w-100 flex gap-4">
@@ -66,8 +66,8 @@ export default function SignIn() {
               </button>
             </Form.Submit>
           </div>
-        </div>
-      </Form.Root>
+        </Form.Root>
+      </div>
     </main>
   );
 }
