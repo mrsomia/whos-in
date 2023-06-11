8e0ba5c80f2a2ec2175def63b3bb96439dc6188a
Sachin Somia <sachinsomia@gmail.com>
2023-06-11 17:11:31 +0100

    Fix: Moves Dashboard to new spinner

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,7 +7,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/Card";
+import Spinner from "@/components/Spinner";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Page() {
@@ -20,31 +22,16 @@ export default function Page() {
   });
 
   if (session.status === "loading") {
-    return (
-      <main className="w-100 flex justify-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="currentColor"
-          className="loading h-6 w-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-          />
-        </svg>
-      </main>
-    );
+    return <Spinner />;
   }
   return (
     <main className="flex w-screen flex-col items-center justify-center">
       <h1>signed in as {session.data.user?.email}</h1>
       <Card title="Upcoming events">
         <CardHeader>
-          <CardTitle>Upcoming events</CardTitle>
+          <Link href="/events">
+            <CardTitle>Upcoming events</CardTitle>
+          </Link>
           <CardDescription>Lorem psum dolor sit amet.</CardDescription>
         </CardHeader>
         <CardBody>
@@ -58,7 +45,9 @@ export default function Page() {
       </Card>
       <Card title="Upcoming events">
         <CardHeader>
-          <CardTitle>Groups</CardTitle>
+          <Link href="/groups">
+            <CardTitle>Groups</CardTitle>
+          </Link>
         </CardHeader>
         <CardBody>
           <div>
