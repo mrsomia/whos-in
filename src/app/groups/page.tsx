421e3bdc9cd0a50ba795dfd61ac15921f13d28bd
Sachin Somia <sachinsomia@gmail.com>
2023-06-11 17:24:02 +0100

    Feat: Adds group pages

---
+++ src/app/groups/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Spinner from "@/components/Spinner";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+export default function Page() {
+  const router = useRouter();
+  const session = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/signin");
+    },
+  });
+
+  if (session.status === "loading") {
+    return <Spinner />;
+  }
+
+  return (
+    <main className="flex flex-col items-center">
+      <h1>This is the events page</h1>
+    </main>
+  );
+}
