8a0b36d48156fd309c0194d25ec74c4d4965e527
Sachin Somia <sachinsomia@gmail.com>
2023-06-17 23:49:49 +0100

    Style(groups-page): Initial styles

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import Spinner from "@/components/Spinner";
+import { Plus } from "lucide-react";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Page() {
@@ -19,7 +21,19 @@ export default function Page() {
 
   return (
     <main className="flex flex-col items-center">
-      <h1>This is the groups page</h1>
+      <div className="m-4 w-[360px] rounded-lg border border-gray-500 p-4">
+        <div className="flex justify-between ">
+          <h1 className="text-xl font-bold md:text-2xl">Groups</h1>
+          <Link href="/groups/new">
+            <button className="rounded-md bg-orange-600 p-2">
+              <Plus />
+            </button>
+          </Link>
+        </div>
+        <div>
+          <p>You are not in any groups, join one or create one</p>
+        </div>
+      </div>
     </main>
   );
 }
