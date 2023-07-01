c5a95afe91c88ad203f2f7b8373cee2da1fd9b8a
Sachin Somia <sachinsomia@gmail.com>
2023-07-01 16:49:01 +0100

    WIP: Intiial new events page

--- src/app/events/new/page.tsx
+++ src/app/events/new/page.tsx
@@ -19,7 +19,37 @@ export default function Page() {
 
   return (
     <main className="flex flex-col items-center">
-      <h1>This is the new events page</h1>
+      <div className="m-4 w-[360px] rounded-lg border border-gray-500 p-4">
+        <div className="flex items-center justify-between">
+          <h1 className="text-xl font-bold md:text-2xl">New Event</h1>
+        </div>
+        <div>
+          <form>
+            <div>
+              <label htmlFor="eventName">Name</label>
+              <input id="eventName" type="text" />
+            </div>
+            <div>
+              <label htmlFor="date">date</label>
+              <input id="date" type="date" />
+            </div>
+            <div>
+              {/* Not sure if this is needed */}
+              <label htmlFor="time">time</label>
+              <input id="time" type="time" />
+            </div>
+            <div>
+              <label htmlFor="location">location</label>
+              <input id="location" type="text" />
+            </div>
+            <div>
+              <label htmlFor="description">description</label>
+              <textarea id="description" />
+            </div>
+            <button>Submit</button>
+          </form>
+        </div>
+      </div>
     </main>
   );
 }
