5492ab406beb3a07edbcb6c1390bd012884111d8
Sachin Somia <sachinsomia@gmail.com>
2023-07-02 17:14:49 +0100

    Style(new-event-page): Adds radix-ui & base styles

--- src/app/events/new/page.tsx
+++ src/app/events/new/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Spinner from "@/components/Spinner";
+import * as Form from "@radix-ui/react-form";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
@@ -24,30 +25,54 @@ export default function Page() {
           <h1 className="text-xl font-bold md:text-2xl">New Event</h1>
         </div>
         <div>
-          <form>
-            <div>
-              <label htmlFor="eventName">Name</label>
-              <input id="eventName" type="text" />
-            </div>
-            <div>
-              <label htmlFor="date">date</label>
-              <input id="date" type="date" />
-            </div>
-            <div>
-              {/* Not sure if this is needed */}
-              <label htmlFor="time">time</label>
-              <input id="time" type="time" />
-            </div>
-            <div>
-              <label htmlFor="location">location</label>
-              <input id="location" type="text" />
-            </div>
-            <div>
-              <label htmlFor="description">description</label>
-              <textarea id="description" />
-            </div>
-            <button>Submit</button>
-          </form>
+          <Form.Root className="my-4 flex flex-col items-center justify-between">
+            <Form.Field className="min-w-full" name="eventName">
+              <div className="flex items-baseline justify-between">
+                <Form.Label className="">Name</Form.Label>
+                <Form.Message className="" match="valueMissing">
+                  Please enter an event name
+                </Form.Message>
+              </div>
+              <Form.Control asChild>
+                <input type="text" />
+              </Form.Control>
+            </Form.Field>
+            <Form.Field className="" name="date">
+              <div className="flex items-baseline justify-between">
+                <Form.Label>Date</Form.Label>
+                <Form.Message className="" match="valueMissing">
+                  Please enter an event date
+                </Form.Message>
+              </div>
+              <Form.Control asChild>
+                <input type="datetime-local" />
+              </Form.Control>
+            </Form.Field>
+            <Form.Field className="" name="location">
+              <div className="flex items-baseline justify-between">
+                <Form.Label>Location</Form.Label>
+                <Form.Message match="valueMissing">
+                  Please enter an event location
+                </Form.Message>
+              </div>
+              <Form.Control asChild>
+                <input type="text" />
+              </Form.Control>
+            </Form.Field>
+            <Form.Field name="description">
+              <div className="flex items-baseline justify-between">
+                <Form.Label>Description</Form.Label>
+              </div>
+              <Form.Control asChild>
+                <textarea />
+              </Form.Control>
+            </Form.Field>
+            <Form.Submit asChild>
+              <button className="mt-6 rounded-xl bg-orange-500 p-4">
+                Submit
+              </button>
+            </Form.Submit>
+          </Form.Root>
         </div>
       </div>
     </main>
