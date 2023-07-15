b29615d54b741d8a4a818b2bf25d2903b7e5d5f0
Sachin Somia <sachinsomia@gmail.com>
2023-07-15 13:10:47 +0100

    Feat(new-events): Adds Frequency Select Menu

--- src/app/events/new/page.tsx
+++ src/app/events/new/page.tsx
@@ -2,6 +2,7 @@
 
 import Spinner from "@/components/Spinner";
 import * as Form from "@radix-ui/react-form";
+import * as Select from "@radix-ui/react-select";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
@@ -45,6 +46,7 @@ export default function Page() {
                   />
                 </Form.Control>
               </Form.Field>
+
               <Form.Field
                 className="flex w-full flex-col space-y-1"
                 name="date"
@@ -63,6 +65,57 @@ export default function Page() {
                   />
                 </Form.Control>
               </Form.Field>
+
+              <Form.Field
+                name="repeat"
+                className="flex w-full flex-col space-y-2"
+                aria-required="true"
+              >
+                <div className="flex items-baseline justify-between">
+                  <Form.Label>Repeat Frequency</Form.Label>
+                </div>
+                <Form.Control asChild>
+                  <Select.Root defaultValue="None">
+                    <Select.Trigger
+                      className="bg-white py-2 text-black"
+                      aria-label="Repeat"
+                    >
+                      <Select.Value placeholder="None" className="" />
+                      <Select.Icon className="text-black"></Select.Icon>
+                    </Select.Trigger>
+
+                    <Select.Portal>
+                      <Select.Content
+                        side="bottom"
+                        position="popper"
+                        className="w-[262px] rounded-s bg-slate-700 p-2"
+                      >
+                        <Select.Viewport className="p-2">
+                          <Select.Item value="None" className="p-1 py-2">
+                            <Select.ItemText>None</Select.ItemText>
+                          </Select.Item>
+                          <Select.Item value="Daily" className="p-1 py-2">
+                            <Select.ItemText>Daily</Select.ItemText>
+                          </Select.Item>
+                          <Select.Item value="Weekly" className="p-1 py-2">
+                            <Select.ItemText>Weekly</Select.ItemText>
+                          </Select.Item>
+                          <Select.Item
+                            value="Every 2 Weeks"
+                            className="p-1 py-2"
+                          >
+                            <Select.ItemText>Every 2 Weeks</Select.ItemText>
+                          </Select.Item>
+                          <Select.Item value="Monthly" className="p-1 py-2">
+                            <Select.ItemText>Monthly</Select.ItemText>
+                          </Select.Item>
+                        </Select.Viewport>
+                      </Select.Content>
+                    </Select.Portal>
+                  </Select.Root>
+                </Form.Control>
+              </Form.Field>
+
               <Form.Field
                 className="flex w-full flex-col space-y-1"
                 name="location"
@@ -81,6 +134,7 @@ export default function Page() {
                   />
                 </Form.Control>
               </Form.Field>
+
               <Form.Field
                 className="flex w-full flex-col space-y-1"
                 name="description"
@@ -92,6 +146,7 @@ export default function Page() {
                   <textarea className="rounded-s p-2 text-black" />
                 </Form.Control>
               </Form.Field>
+
               <div className="pt-6">
                 <Form.Submit asChild>
                   <button className="rounded-xl bg-orange-500 px-6 py-2">
