54824630e2215ac1e249b6097b97d0132abaa220
Sachin Somia <sachinsomia@gmail.com>
2023-08-21 19:46:38 +0100

    refactor(new events): Moves to RFC

---
+++ src/components/NewEventsForm.tsx
@@ -0,0 +1,174 @@
+"use client";
+
+import * as Form from "@radix-ui/react-form";
+import * as Select from "@radix-ui/react-select";
+
+const groups = [
+  { id: 1, name: "Poker night" },
+  { id: 2, name: "Fifa tournament" },
+  { id: 3, name: "Games night" },
+];
+
+export default function NewEventsFrom() {
+  return (
+    <>
+      <Form.Root className="my-4 flex w-4/5 flex-col items-center justify-between space-y-4">
+        <Form.Field className="flex w-full flex-col space-y-1" name="eventName">
+          <div className="flex items-baseline justify-between">
+            <Form.Label className="">Name</Form.Label>
+            <Form.Message className="text-red-400" match="valueMissing">
+              Please enter an event name
+            </Form.Message>
+          </div>
+          <Form.Control asChild>
+            <input
+              type="text"
+              required
+              className="w-full rounded-s p-2 text-black"
+            />
+          </Form.Control>
+        </Form.Field>
+
+        <Form.Field className="flex w-full flex-col space-y-1" name="date">
+          <div className="flex items-baseline justify-between">
+            <Form.Label>Date</Form.Label>
+            <Form.Message className="text-red-400" match="valueMissing">
+              Please enter an event date
+            </Form.Message>
+          </div>
+          <Form.Control asChild>
+            <input
+              type="datetime-local"
+              required
+              className="w-full rounded-s p-2 text-black"
+            />
+          </Form.Control>
+        </Form.Field>
+
+        <Form.Field
+          name="repeat"
+          className="flex w-full flex-col space-y-2"
+          aria-required="true"
+        >
+          <div className="flex items-baseline justify-between">
+            <Form.Label>Repeat Frequency</Form.Label>
+          </div>
+          <Form.Control asChild>
+            <Select.Root defaultValue="None">
+              <Select.Trigger
+                className="bg-white py-2 text-black"
+                aria-label="Repeat"
+              >
+                <Select.Value placeholder="None" className="" />
+                <Select.Icon className="text-black"></Select.Icon>
+              </Select.Trigger>
+
+              <Select.Portal>
+                <Select.Content
+                  side="bottom"
+                  position="popper"
+                  className="w-[262px] rounded-s bg-slate-700 p-2"
+                >
+                  <Select.Viewport className="p-2">
+                    <Select.Item value="None" className="p-1 py-2">
+                      <Select.ItemText>None</Select.ItemText>
+                    </Select.Item>
+                    <Select.Item value="Daily" className="p-1 py-2">
+                      <Select.ItemText>Daily</Select.ItemText>
+                    </Select.Item>
+                    <Select.Item value="Weekly" className="p-1 py-2">
+                      <Select.ItemText>Weekly</Select.ItemText>
+                    </Select.Item>
+                    <Select.Item value="Every 2 Weeks" className="p-1 py-2">
+                      <Select.ItemText>Every 2 Weeks</Select.ItemText>
+                    </Select.Item>
+                    <Select.Item value="Monthly" className="p-1 py-2">
+                      <Select.ItemText>Monthly</Select.ItemText>
+                    </Select.Item>
+                  </Select.Viewport>
+                </Select.Content>
+              </Select.Portal>
+            </Select.Root>
+          </Form.Control>
+        </Form.Field>
+
+        <Form.Field
+          name="group"
+          className="flex w-full flex-col space-y-2"
+          aria-required="true"
+        >
+          <div className="flex items-baseline justify-between">
+            <Form.Label>Group</Form.Label>
+          </div>
+          <Form.Control asChild required>
+            <Select.Root>
+              <Select.Trigger
+                className="bg-white py-2 text-black"
+                aria-label="Group"
+              >
+                <Select.Value placeholder="" />
+                <Select.Icon className="text-black"></Select.Icon>
+              </Select.Trigger>
+
+              <Select.Portal>
+                <Select.Content
+                  side="bottom"
+                  position="popper"
+                  className="w-[262px] rounded-s bg-slate-700 p-2"
+                >
+                  <Select.Viewport className="p-2">
+                    {groups.map((group) => (
+                      <Select.Item
+                        key={group.id}
+                        value={group.id.toString()}
+                        className="p-1 py-2"
+                      >
+                        <Select.ItemText>{group.name}</Select.ItemText>
+                      </Select.Item>
+                    ))}
+                  </Select.Viewport>
+                </Select.Content>
+              </Select.Portal>
+            </Select.Root>
+          </Form.Control>
+        </Form.Field>
+
+        <Form.Field className="flex w-full flex-col space-y-1" name="location">
+          <div className="flex items-baseline justify-between">
+            <Form.Label>Location</Form.Label>
+            <Form.Message className="text-red-400" match="valueMissing">
+              Please enter an event location
+            </Form.Message>
+          </div>
+          <Form.Control asChild>
+            <input
+              type="text"
+              required
+              className="w-full rounded-s p-2 text-black"
+            />
+          </Form.Control>
+        </Form.Field>
+
+        <Form.Field
+          className="flex w-full flex-col space-y-1"
+          name="description"
+        >
+          <div className="flex items-baseline justify-between">
+            <Form.Label>Description</Form.Label>
+          </div>
+          <Form.Control asChild>
+            <textarea className="rounded-s p-2 text-black" />
+          </Form.Control>
+        </Form.Field>
+
+        <div className="pt-6">
+          <Form.Submit asChild>
+            <button className="rounded-xl bg-orange-500 px-6 py-2">
+              Submit
+            </button>
+          </Form.Submit>
+        </div>
+      </Form.Root>
+    </>
+  );
+}
