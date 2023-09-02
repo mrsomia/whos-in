1c66e3c01c75843a1e556c5db4468d35a3bb2cb8
Sachin Somia <sachinsomia@gmail.com>
2023-09-02 19:13:47 +0100

    Fix: Removes commented out next auth code

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,16 +3,4 @@ import NextAuth from "next-auth/next";
 
 const handler = NextAuth(authOptions);
 
-// export async function GET(request: Request) {
-//   console.log(request.url);
-//   console.log(request);
-//   return await handler(request);
-// }
-
-// export async function POST(request: Request) {
-//   console.log(request.url);
-//   console.log(request);
-//   return await handler(request);
-// }
-
 export { handler as GET, handler as POST };
